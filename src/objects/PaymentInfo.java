e825070e7ba3998c360f7fc966772fe93d1a72a6
damonmaz <>
2024-11-14 19:22:10 -0700

    Added constructors, getters, setters

--- src/objects/PaymentInfo.java
+++ src/objects/PaymentInfo.java
@@ -3,16 +3,60 @@ public class PaymentInfo {
     //---------------//
     //   Variables   //
     //---------------//
-
+    private double cardNumber;
+    private String expiration;
+    private int cv;
+    private String fullName;
+    
     //---------------//
     //  Constructor  //
     //---------------//
+    public PaymentInfo(double cardNumber, String expiration, int cv, String fullName) {
+        this.cardNumber = cardNumber;
+        this.expiration = expiration;
+        this.cv = cv;
+        this.fullName = fullName;
+    }
 
     //---------------------//
     //  Getters + Setters  //
     //---------------------//
+    public double getCardNumber() {
+        return cardNumber;
+    }
 
+    public void setCardNumber(double cardNumber) {
+        assert(String.valueOf(cardNumber).length() == 16);
+        this.cardNumber = cardNumber;
+    }
+
+    public String getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(String expiration) {
+        // Create assert statement for expiration (might have to change to a DateTime type of object)
+        this.expiration = expiration;
+    }
+
+    public int getCV() {
+        return cv;
+    }
+
+    public void setCV(int cv) {
+        assert(cv >= 100 && cv <= 999);
+        this.cv = cv;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
     //-------------//
     //   Methods   //
     //-------------//
+    
 }
--- src/objects/Ticket.java
+++ src/objects/Ticket.java
@@ -3,14 +3,36 @@ public class Ticket {
     //---------------//
     //   Variables   //
     //---------------//
+    private int ticketID;
+    private Showtime showtime;
+    private Theatre theatre;
+    private Seat seat;
 
     //---------------//
     //  Constructor  //
     //---------------//
+    public Ticket(Showtime showtime, Theatre theatre, Seat seat, int ticketID) {
+        this.ticketID = ticketID;
+        this.showtime = showtime;
+        this.theatre = theatre;
+        this.seat = seat;
+    }
 
     //---------------------//
     //  Getters + Setters  //
     //---------------------//
+    public int getTicketID() { return ticketID; }
+    public void setTicketID(int ticketID) { this.ticketID = ticketID; }
+
+    public Showtime getShowtime() { return showtime; }
+    public void setShowtime(Showtime showtime) { this.showtime = showtime; }
+
+    public Theatre getTheatre() { return theatre; }
+    public void setTheatre(Theatre theatre) { this.theatre = theatre; }
+
+    public Seat getSeat() { return seat; }
+    public void setSeat(Seat seat) { this.seat = seat; }
+
 
     //-------------//
     //   Methods   //
