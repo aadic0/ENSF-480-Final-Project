de9acc5a434aa96f8f2db5301ed07bb1c0561751
damonmaz <>
2024-11-19 20:08:30 -0700

    added way to connect to SQL database

--- src/objects/control/DatabaseController.java
+++ src/objects/control/DatabaseController.java
@@ -1,5 +1,34 @@
 package objects.control;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Singleton class representing SQL database
+ */
 public class DatabaseController {
-    
+    private static String username = "theatre_connect";
+    private static String password = "theatre";
+    private static String url = "jdbc:mysql://localhost/THEATRE_DB";
+    private static Connection connection = null;
+
+    /**
+     * Connect to Theatre SQL database
+     * @return Connection to database
+     * @throws SQLException
+     */
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
+    /**
+     * Disconnect from Theatre SQL database
+     * @throws SQLException
+     */
+    public static void closeConnection() throws SQLException {
+        if (connection != null && connection.isClosed() == false) {
+            connection.close();
+        }
+    }
+
 }
