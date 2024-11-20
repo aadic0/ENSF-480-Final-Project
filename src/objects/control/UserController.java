de812a7552b76cda703ae489f730fb64ef1b2973
damonmaz <>
2024-11-19 22:38:33 -0700

    Got createUser working

--- src/objects/control/UserController.java
+++ src/objects/control/UserController.java
@@ -1,5 +1,9 @@
 package objects.control;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import objects.entity.RegisteredUser;
 import objects.entity.User;
 
@@ -9,6 +13,8 @@ public class UserController {
  
     /**
      * turn a regular user into a registered user
+     * @param user
+     * @return RegisteredUser object (may not keep)
      */
     public RegisteredUser createRegisteredUser(User user) {
         RegisteredUser regUser = new RegisteredUser(
@@ -24,15 +30,30 @@ public class UserController {
     }
 
     /**
-     * Create a user object and add to database
+     * Create a user object and add to database (MIGHT CHANGE, IDK IF WE EVEN WANT TO ADD NON-REGISTERED USERS TO DB)
      * @param email
      * @param pwd
+     * @return user object (may not keep)
      */
-    public User createUser(String email, String pwd){
-        User user = new User(email);
+    public User createUser(String email, String pwd) {
+        // Try to connect to database
+        try (Connection connection = DatabaseController.getConnection();) { 
+            
+            // Prepare query
+            String query = "INSERT INTO DEFAULT_USER(Email, Pwd) VALUES(?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
 
-        // Add to database
-        // ...
+            preparedStatement.setString(1, email); // Changes '?' to the text
+            preparedStatement.setString(2, pwd);
+            
+            // Execute Query
+            // preparedStatement.executeQuery();
+            preparedStatement.executeUpdate();
+            
+        }
+        catch (SQLException e) { return null; }
+
+        User user = new User(email);
 
         return user;
     }
