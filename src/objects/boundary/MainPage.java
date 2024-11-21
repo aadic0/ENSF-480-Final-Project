ee013b6ebe9cfe39d75be7d3ecf7d366d109f786
daadhaymour <135389387+daadhaymour@users.noreply.github.com>
2024-11-21 16:40:32 -0700

    added boundary classes

---
+++ src/objects/boundary/MainPage.java
@@ -0,0 +1,6 @@
+//main page where all the movies are, nav bar(user,cart), search bar
+package objects.boundary;
+
+public class MainPage {
+    
+}
---
+++ src/objects/boundary/Register.java
@@ -0,0 +1,5 @@
+package objects.boundary;
+
+public class Register {
+    
+}
