105833121b0af6c72afde0846f2938ff037fa2e3
damonmaz <>
2024-11-22 00:03:27 -0700

    modified ShowtimeController method to show all showtimes, added testing in main

--- src/objects/control/Main.java
+++ src/objects/control/Main.java
@@ -1,41 +1,37 @@
 package objects.control;
 
-import objects.entity.User;
-
 // NOTE: I'm putting this file in the src/objects/control folder for now because it is easier to compile/
 //       It should not stay in here long term
 
 // NOTE: I'm using this file to test out database communication, this will (probably) not be the actual main we use
 public class Main {
     public static void main(String[] args) {
-    /* UserController testing */
-        // UserController userController = new UserController();
 
-        // userController.createUser("test@gobo.com", "pwd");
-        // userController.createRegisteredUser(new User("test@gobo.com"), "pwd", 
-        // "Borgen", "Schmorgen", 
-        // "123 abc", "Salt Lake City", "Manitoba", "T2P0G5");
-
-    /* ShowtimeController Testing */
-        // ShowtimeController showtimeController = new ShowtimeController();
-        // showtimeController.showShowtimes();
-
-    RegisteredUserController controller = new RegisteredUserController();
+    //------------------------------//
+    //   RegisteredUserController   //
+    //------------------------------//
+    // RegisteredUserController regUserController = new RegisteredUserController();
     
-    // Test the method
-    String testEmail = "test@test.com";
-    String testPassword = "jdoe";
-    boolean isAuthenticated = controller.authenticateUser(testEmail, testPassword);
+    // // Test the method
+    // String testEmail = "test@test.com";
+    // String testPassword = "jdoe";
+    // boolean isAuthenticated = regUserController.authenticateUser(testEmail, testPassword);
     
-    System.out.println("Authentication result: " + isAuthenticated);
+    // System.out.println("Authentication result: " + isAuthenticated);
 
+    // regUserController.registerUser("aadichauhan321@gmail.com", "deezNutz", "Aadi", "Chauhan", "93 Legacy Common SE", "Calgary", "Alberta", "T2X 2A9");
+    // regUserController.updateAddressInfo("Fake address");
+    // regUserController.updateName("Aaditya", "Chonuts");
+    // regUserController.updateEmailInfo("deeznuts@gmail.com");
 
-    controller.registerUser("aadichauhan321@gmail.com", "deezNutz", "Aadi", "Chauhan", "93 Legacy Common SE", "Calgary", "Alberta", "T2X 2A9");
-    
-    controller.updateAddressInfo("Fake address");
-    controller.updateName("Aaditya", "Chonuts");
-    
-    controller.updateEmailInfo("deeznuts@gmail.com");
+    //------------------------//
+    //   ShowtimeController   //
+    //------------------------//
+    ShowtimeController showtimeController = new ShowtimeController();
+    showtimeController.getAllShowtimes();
+
 
     }
+
+    
 }
--- src/objects/control/ShowtimeController.java
+++ src/objects/control/ShowtimeController.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 
 public class ShowtimeController {
     public ShowtimeController(){}
@@ -11,27 +12,39 @@ public class ShowtimeController {
     /**
      * Show all showtime IDs from SQL database
      */
-    public void showShowtimes() {
+    public void getAllShowtimes() {
         
         // Try to connect to database
         try (Connection connection = DatabaseController.createConnection()) {
             
             // Prepare query
-            String query = "SELECT * FROM showtime;";
+            String query = "SELECT s.ShowDateTime, m.Title AS MovieTitle, t.TheatreName, tr.RoomName " +
+                           "FROM SHOWTIME s " +
+                           "JOIN MOVIE m ON s.MovieTitle = m.Title " +
+                           "JOIN THEATREROOM tr ON s.TheatreRoomID = tr.TheatreRoomID " +
+                           "JOIN THEATRE t ON tr.TheatreID = t.TheatreID;";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             
             // Execute Query
             ResultSet resultSet = preparedStatement.executeQuery();
-
+    
             while (resultSet.next()) {
-                int showtimeID = resultSet.getInt("ShowtimeID"); 
-                System.out.println("ShowtimeID: " + showtimeID);
+                String movieTitle = resultSet.getString("MovieTitle");
+                Timestamp showDateTime = resultSet.getTimestamp("ShowDateTime");
+                String theatreName = resultSet.getString("TheatreName");
+                String roomName = resultSet.getString("RoomName");
+                
+                System.out.println("Movie: " + movieTitle);
+                System.out.println("Time: " + showDateTime);
+                System.out.println("Theatre: " + theatreName);
+                System.out.println("Room: " + roomName);
+                System.out.println("-----------");
             }
-
-            
+        
         }
         catch (SQLException e) {
             System.out.println(e);
         }
     }
+    
 }
