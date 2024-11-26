c9c3f46367af8f8a840e57ddf807518d8bb84ccc
damonmaz <>
2024-11-26 15:42:52 -0700

    Updated functionality and comments of buyTicketTest()

--- src/objects/control/Main.java
+++ src/objects/control/Main.java
@@ -58,6 +58,11 @@ public class Main {
     // }
 
     public static void buyTicketTest() {
+        //NOTE:
+        // This test sucks. You need to manually change showtimeID values depending on what you want to test.
+        // If this test doesn't work, it's almost certainly because some showtime value is messed up in the DB
+
+
         // Mock data for testing
         String email = "testuser@example.com";
     
@@ -74,37 +79,17 @@ public class Main {
         // Create a mock Showtime object (for example, "The Dark Knight" at Room 1, 2024-11-23 18:30:00)
         Movie movie = new Movie(10, "Devonian Park", "Adventure", "PG-13", Time.valueOf("2:01:00"));
         TheatreRoom theatreRoom = new TheatreRoom(1, new Theatre(1, "ACMEplex Theatre", "123 Main Street, Calgary, AB"), "Room A");
-        Showtime showtime = new Showtime(20, movie, theatreRoom, Timestamp.valueOf("2024-11-25 14:30:00"));
+        Showtime showtime = new Showtime(40, movie, theatreRoom, Timestamp.valueOf("2024-11-30 17:00:00"));
     
         // Simulate the ticket purchase
         TicketController ticketController = new TicketController();
         ticketController.purchaseTicket(email, seat, showtime, paymentInfo, 15.99f);
     
-        // Verify if the ticket was added to the database
-        try (Connection connection = DatabaseController.createConnection()) {
-            String query = "SELECT * FROM TICKET WHERE ShowtimeID = ?";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-                preparedStatement.setInt(1, 1);
-    
-                var resultSet = preparedStatement.executeQuery();
-                if (resultSet.next()) {
-                    System.out.println("Ticket purchase was successful!");
-                    System.out.println("Ticket Details:");
-                    System.out.println("TicketID: " + resultSet.getInt("TicketID"));
-                    System.out.println("SeatID: " + resultSet.getInt("SeatID"));
-                    System.out.println("PurchaseDateTime: " + resultSet.getTimestamp("PurchaseDateTime"));
-                } else {
-                    System.out.println("Ticket purchase failed.");
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     public static void refundTicketTest() {
         TicketController tc = new TicketController();
-        tc.refundTicket(7, "testuser@gmail.com", null);
+        tc.refundTicket(1, "testuser@example.com", null);
     }
     
 
