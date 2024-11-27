69682a2ad9f76856fef007a91f94612757c60fff
damonmaz <>
2024-11-26 22:02:00 -0700

    Added sendPublicShowTimeAnnouncement method

--- src/objects/control/AnnouncementController.java
+++ src/objects/control/AnnouncementController.java
@@ -31,7 +31,39 @@ public class AnnouncementController{
             // preparedStatement.setInt(1, announcementID);
             preparedStatement.setBoolean(1, IS_PUBLIC);
             preparedStatement.setString(2, message);
-            preparedStatement.setTimestamp(3, new Timestamp(System.currentTimeMillis())); // Convert String to SQL Date
+            preparedStatement.setTimestamp(3, new Timestamp(System.currentTimeMillis())); 
+            preparedStatement.setInt(4, (showTimeID));
+
+            // Execute the insert query
+            int rowsAffected = preparedStatement.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Announcement added successfully!");
+            } else {
+                System.out.println("Failed to add the announcement.");
+            }
+        } 
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * email an announcement to a list of registered users about early access showtimes
+     * @param announcement
+     * @param registeredUsersList
+     */
+    public void sendPublicShowTimeAnnouncement(String message, int showTimeID) {
+
+        String insertAnnouncementQuery = "INSERT INTO ANNOUNCEMENT (IsPublic, AnnouncementMessage, DateAnnounced, ShowtimeID) VALUES (?, ?, ?, ?)";
+
+        final boolean IS_PUBLIC = true;
+
+        try (Connection connection = DatabaseController.createConnection(); PreparedStatement preparedStatement = connection.prepareStatement(insertAnnouncementQuery)){     
+            // Set values for the announcement table
+            // preparedStatement.setInt(1, announcementID);
+            preparedStatement.setBoolean(1, IS_PUBLIC);
+            preparedStatement.setString(2, message);
+            preparedStatement.setTimestamp(3, new Timestamp(System.currentTimeMillis())); 
             preparedStatement.setInt(4, (showTimeID));
 
             // Execute the insert query
