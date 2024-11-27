ef12cb7efa5d4e2c5dac8ce418ff302cdcafef99
damonmaz <>
2024-11-27 16:21:19 -0700

    retrieveAllShowtimes() now return a hashmap containing its values

--- src/objects/control/ShowtimeController.java
+++ src/objects/control/ShowtimeController.java
@@ -1,11 +1,14 @@
 package objects.control;
 
+import java.lang.reflect.Type;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Time;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ShowtimeController {
     public ShowtimeController(){}
@@ -16,30 +19,42 @@ public class ShowtimeController {
 //-------------------------------------------------------------//
 
 
+
     /**
-     * Show all showtime IDs from SQL database
+     * 
+     * @param con
+     * @return A hasmap with ShowtimeID as key, and ArrayList with TheatreRoomID in [0], MovieID in [1], and ShowDateTime in [2] as value
      */
-    public void retrieveAllShowtimes(Connection con) {
+    public HashMap<Integer, ArrayList<Object>> retrieveAllShowtimes(Connection con) {
+
+        HashMap<Integer, ArrayList<Object>> showtimeMap = new HashMap<>();
         
         String query = "SELECT ShowtimeID, TheatreRoomID, ShowDateTime, MovieID FROM SHOWTIME;";
-            try (PreparedStatement preparedStatement = con.prepareStatement(query)) {
-                // Execute Query
-                ResultSet resultSet = preparedStatement.executeQuery();
 
-                while (resultSet.next()) {
-                    int showtimeID = resultSet.getInt("ShowtimeID");
-                    int TheatreRoomID = resultSet.getInt("TheatreRoomID");
-                    Timestamp showDateTime = resultSet.getTimestamp("ShowDateTime");
-                    int movieID = resultSet.getInt("MovieID");
+        // Query - Get all information from SHOWTIME table and add it to a Hashmap
+        try (PreparedStatement preparedStatement = con.prepareStatement(query)) {
+    
+            ResultSet resultSet = preparedStatement.executeQuery();
 
-                    System.out.println("showtimeID " + showtimeID);
-                    System.out.println("TheatreRoomID " + TheatreRoomID);
-                    System.out.println("showDateTime " + showDateTime);
-                    System.out.println("movieID " + movieID);
-                    System.out.println("-----------");
-                }
+            while (resultSet.next()) {
+
+                ArrayList<Object> valueArrayList = new ArrayList<>();
+
+                int showtimeID = resultSet.getInt("ShowtimeID");
+                int TheatreRoomID = resultSet.getInt("TheatreRoomID");
+                Timestamp showDateTime = resultSet.getTimestamp("ShowDateTime");
+                int movieID = resultSet.getInt("MovieID");
+
+                // Values in ArrayList
+                valueArrayList.add(TheatreRoomID); // [0]
+                valueArrayList.add(movieID);       // [1]
+                valueArrayList.add(showDateTime);  // [2]
+                
+                showtimeMap.put(showtimeID, valueArrayList);
+            }
         } catch (SQLException e) { System.out.println(e); }
 
+        return showtimeMap;
     }
 
 //-------------------------------------------------------------//
