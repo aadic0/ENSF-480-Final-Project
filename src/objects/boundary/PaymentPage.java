39d98bdd4aa21eae97ef1e3a302d212331e66544
T-Smithy <tanis.smith@ucalgary.ca>
2024-11-28 18:23:59 -0700

    added more starter frames

--- src/objects/boundary/PaymentPage.java
+++ src/objects/boundary/PaymentPage.java
@@ -2,6 +2,58 @@
 
 package objects.boundary;
 
-public class PaymentPage {
+import objects.control.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class PaymentPage extends JPanel {
+    //components
+    private JLabel title;
+    //private JTextField searchMovie;
+    //private JButton searchButton;
+
+    //controller
+    //private ShowtimeController showControl;
+
+    private JFrame frame; //reference to parent frame
+
+    //ctor
+    public PaymentPage(JFrame frame){
+        this.frame = frame;
+        //this.showControl = new ShowtimeController(); //add required control obj
+
+        /*panel setup */
+        setLayout(new GridBagLayout()); //arrange components in grid-like structure
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(10,10,10,10);
+       
+        title = new JLabel("Movie Ticket-Booking App");
+        title.setFont(new Font("Arial", Font.BOLD,18));
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.anchor = GridBagConstraints.CENTER;
+        add(title, constraints); //add to the panel
+
+
+    }
     
+
+    //temporary main method for testing
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.add(new PaymentPage(frame));
+        frame.setVisible(true);
+    
+    }
+
 }
--- src/objects/boundary/ViewMovie.java
+++ src/objects/boundary/ViewMovie.java
@@ -7,8 +7,53 @@ import objects.control.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener; 
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class ViewMovie {
+
+public class ViewMovie extends JPanel {
+    //components
+    private JLabel title;
+    //private JTextField searchMovie;
+    //private JButton searchButton;
+
+    //controller
+    //private ShowtimeController showControl;
+
+    private JFrame frame; //reference to parent frame
+
+    //ctor
+    public ViewMovie(JFrame frame){
+        this.frame = frame;
+        //this.showControl = new ShowtimeController(); //add required control obj
+
+        /*panel setup */
+        setLayout(new GridBagLayout()); //arrange components in grid-like structure
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(10,10,10,10);
+       
+        title = new JLabel("Movie Ticket-Booking App");
+        title.setFont(new Font("Arial", Font.BOLD,18));
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.anchor = GridBagConstraints.CENTER;
+        add(title, constraints); //add to the panel
+
+
+    }
     
+
+    //temporary main method for testing
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.add(new ViewMovie(frame));
+        frame.setVisible(true);
+    
+    }
+
 }
